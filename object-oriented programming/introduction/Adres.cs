15d262249a9ab2735c1e943968bc9df617014828
Filip Wróblewski <75478989+filipwroblewski@users.noreply.github.com>
2021-10-15 18:48:36 +0200

    Update Adres.cs

--- object-oriented programming/introduction/Adres.cs
+++ object-oriented programming/introduction/Adres.cs
@@ -1,8 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace zajecia_1
+namespace ConsoleApp1
 {
     class Adres
     {
@@ -30,7 +30,7 @@ namespace zajecia_1
             get => _nrMieszkania;
             set => _nrMieszkania = value;
         }
-        
+
         public string kodPocztowy
         {
             get => _kodPocztowy;
@@ -52,6 +52,35 @@ namespace zajecia_1
         public void OdczytDanych()
         {
             Console.WriteLine($"Ul. {this.ulica} {this.nrDomu}/{this.nrMieszkania}\n{this.kodPocztowy} {this.miasto}\n{this.panstwo}");
+        }
+
+        public void SetUpAddress(int i, string[,] osoby)
+        {
+            Console.WriteLine("\n--- Adres ---");
+
+            Console.Write("Ulica: ");
+            this.ulica = Console.ReadLine();
+            osoby[i, 3] = this.ulica;
+
+            Console.Write("Nr domu: ");
+            this.nrDomu = Console.ReadLine();
+            osoby[i, 4] = this.nrDomu;
+
+            Console.Write("Nr mieszkania: ");
+            this.nrMieszkania = Console.ReadLine();
+            osoby[i, 5] = this.nrMieszkania;
+
+            Console.Write("Kod pocztowy: ");
+            this.kodPocztowy = Console.ReadLine();
+            osoby[i, 6] = this.kodPocztowy;
+
+            Console.Write("Miasto: ");
+            this.miasto = Console.ReadLine();
+            osoby[i, 7] = this.miasto;
+
+            Console.Write("Panstwo: ");
+            this.panstwo = Console.ReadLine();
+            osoby[i, 8] = this.panstwo;
         }
     }
 }
